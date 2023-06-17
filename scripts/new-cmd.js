dbfcad3721b691d3f3082a252660d78b4d6c26c1
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-06-16 19:26:50 -0500

    Add new-cmd script for generating script template

---
+++ scripts/new-cmd.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  throw new Error('Command name is required');
+}
+
+const commandDir = './src/commands';
+const name = process.argv[2];
+const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+const fileName = `${commandDir}/${name}.ts`;
+
+const fileContent = `import {ICommand} from '.';
+import {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  AutocompleteInteraction,
+} from 'discord.js';
+import {injectable} from 'inversify';
+
+@injectable()
+export default class ${capitalizedName}Command implements ICommand {
+  public readonly data = new SlashCommandBuilder()
+    .setName('${name}')
+    .setDescription('${capitalizedName} command');
+
+  public async execute(interaction: ChatInputCommandInteraction) {
+    await interaction.reply('${capitalizedName} command');
+  }
+
+  public autocomplete?: (interaction: AutocompleteInteraction) => Promise<void>;
+}
+`;
+
+if (!fs.existsSync(commandDir)) {
+  fs.mkdirSync(commandDir);
+}
+
+if (fs.existsSync(fileName)) {
+  throw new Error(`Command ${name} already exists`);
+}
+
+fs.writeFileSync(fileName, fileContent);
